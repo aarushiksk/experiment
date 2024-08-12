9110945c5eb35fbaea5a5d2b205584cab453dd29
Aarushi <92772439+aarushiksk@users.noreply.github.com>
2024-08-12 16:09:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
+# from PyPDF2 import PdfReader
+
+# reader = PdfReader("Aarushi_s_Resume_AIML (6).pdf")
+# number_of_pages = len(reader.pages)
+# page = reader.pages[0]
+# text = page.extract_text()
+
+# print(text)
+
+# from fastapi import FastAPI, File, UploadFile
+
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# async def start():
+#     return {"message": "Hello World"}
+
+# @app.post("/pdf")
+# async def read_pdf():
+#     return {"message": "This is a PDF Reader"}
+
+
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from pydantic import BaseModel
 import PyPDF2
 from io import BytesIO
 from fastapi.middleware.cors import CORSMiddleware
 
-
 app = FastAPI()
-
 
 origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:3000",
-    "https://fastapi-example-kw8k.onrender.com",
     "http://localhost:3000/dashboard/resume",
 ]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"],
 )
 # Global variable to store extracted text
 extracted_text_storage = ""
@@ -46,29 +65,8 @@ async def extract_text(file: UploadFile = File(...)):
     
     # Store the extracted text
     extracted_text_storage = text
-    
-    final_answer=""
-    for line in text.split("\n"):
-        if line:
-            final_answer+=line+" "
-    index = final_answer.find("Skills")
-    if index == -1:
-        raise HTTPException(status_code=400, detail="The keyword 'Skills' was not found.")
-        
-    start_index = index + len("Skills")
-    end_index = final_answer.find("Projects")
-        
-    if end_index == -1:
-            extracted_text = final_answer[start_index:].strip()
-    else:
-            extracted_text = final_answer[start_index:end_index].strip()
-        
-    if not extracted_text:
-        raise HTTPException(status_code=400, detail="No text found between 'Skills' and 'Projects'.")
-        
-    return TextResponse(text=extracted_text)
 
-
+    return TextResponse(text=text)
 
 @app.get("/extracted-text", response_model=TextResponse)
 async def get_extracted_text():
